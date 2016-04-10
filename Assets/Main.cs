4889dc8a240d5dd2f0a424e278d1d96f543a3388
limbermuffin <limbermuffin@gmail.com>
2016-04-10 16:16:54 -0700

    Revert "Stopped redefining body each from and cleaned up scripts"
    This reverts commit 9b151af5a32663637b1df239c68edcb6ba5a09e1.

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -5,13 +5,10 @@ public class Main : MonoBehaviour {
 
     public static GameObject player;
     public float movementSpeed = 10;
-    public Rigidbody body;
-
     // Use this for initialization
     void Start()
     {
         player = GameObject.Find("Player");
-        body = player.GetComponent<Rigidbody>();
         CameraController.Target = player.transform;
     }
 
@@ -27,12 +24,13 @@ public class Main : MonoBehaviour {
                 {
                     player = hit.transform.gameObject;
                     CameraController.Target = player.transform;
-                    body = player.GetComponent<Rigidbody>();
                 }
         }
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        body.AddForce(movement * movementSpeed);
+        player.GetComponent<Rigidbody>().AddForce(movement * movementSpeed);
+
     }
 }
