1deb7d4c78e09ce0944de24226033d0afa97be98
limbermuffin <limbermuffin@gmail.com>
2016-04-10 17:19:05 -0700

    Variables now have appropriate names

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Main : MonoBehaviour {
 
     public static GameObject player;
-    public GameObject camerass;
+    public GameObject playerCamera;
     public Rigidbody body;
     public float movementSpeed = 10;
 
@@ -12,7 +12,7 @@ public class Main : MonoBehaviour {
     void Start()
     {
         player = GameObject.Find("Player");
-        camerass = GameObject.Find("Main Camera");
+        playerCamera = GameObject.Find("Main Camera");
         body = player.GetComponent<Rigidbody>();
         CameraController.Target = player.transform;
     }
@@ -20,24 +20,22 @@ public class Main : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
-        {
+        if (Input.GetMouseButtonDown(1)){
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100))
+            {
                 if (hit.transform.gameObject.tag == "Player")
                 {
                     player = hit.transform.gameObject;
                     CameraController.Target = player.transform;
                     body = player.GetComponent<Rigidbody>();
                 }
+            }
         }
-
-        float moveHorizontal = Mathf.Cos(camerass.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Vertical") - Mathf.Sin(camerass.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Horizontal");
-        float moveVertical = Mathf.Sin(camerass.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Vertical") + Mathf.Cos(camerass.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Horizontal");
-        float goat = camerass.transform.eulerAngles.x;
+        float moveHorizontal = Mathf.Cos(playerCamera.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Vertical") - Mathf.Sin(playerCamera.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Horizontal");
+        float moveVertical = Mathf.Sin(playerCamera.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Vertical") + Mathf.Cos(playerCamera.transform.eulerAngles.y * Mathf.Deg2Rad) * Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(moveVertical, 0.0f, moveHorizontal);
         body.AddForce(movement * movementSpeed);
-
     }
 }
